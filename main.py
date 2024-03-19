3ea573fdd9234c6c88a47773953aec84486686b4
KsyLova <cenylova@gmail.com>
2024-03-19 14:29:32 +0300

    gitignore

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+
 from flask import Flask
 from flask_bootstrap import Bootstrap5
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 flask_app = Flask(__name__)
 flask_app.config['SECRET_KEY'] = "hard to unlock"
+flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/flask_education'
+flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 from app import routes
 
@@ -11,3 +18,7 @@ if __name__ == "__main__":
     flask_app.run(debug=True)
 
 bootstrap = Bootstrap5(flask_app)
+db = SQLAlchemy(flask_app)
+
+
+
